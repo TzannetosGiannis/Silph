f57ec796750205501b6bf062deaf25933e5c9cab
TzannetosGiannis <tzannetosg@gmail.com>
2026-01-16 11:41:55 +0200

    Return biometric min index output

--- benchmarks/biometric/512/biometric.c
+++ benchmarks/biometric/512/biometric.c
@@ -17,11 +17,6 @@
 #define N 512
 #define K 4
 
-typedef struct {
-    int min_sum;
-    int min_index;
-} Output;
-
 /* Compute squared Euclidean distance between two K-dimensional vectors */
 int match_fix(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4) {
     int t1 = (x1 - y1);
@@ -32,41 +27,29 @@ int match_fix(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4) {
     return r;
 }
 
-/* Find minimum value and index in array */
-Output min_with_index(int *data, int len) {
-    Output output;
-    output.min_sum = data[0];
-    output.min_index = 0;
-
-    for (int i = 1; i < len; i++) {
-        if (data[i] < output.min_sum) {
-            output.min_sum = data[i];
-            output.min_index = i;
-        }
-    }
-    return output;
-}
-
-/* Compute distances from sample to all database entries */
-void match_decomposed(int *db, int *OUTPUT_matches, int len, int *sample) {
-    for (int i = 0; i < len; i++) {
-        OUTPUT_matches[i] = match_fix(
-            db[i*K], db[i*K+1], db[i*K+2], db[i*K+3],
-            sample[0], sample[1], sample[2], sample[3]
-        );
-    }
-}
-
-Output main(
+int main(
     __attribute__((private(0))) int db[N * K],   /* Database: N entries x K features */
-    __attribute__((private(1))) int sample[K]    /* Query: K features to match */
+    __attribute__((private(1))) int sample[K],   /* Query: K features to match */
+    __attribute__((public)) int result[2]        /* Output: min_sum, min_index */
 )
 {
-    int matches[N];
+    int min_sum = 0;
+    int min_index = 0;
 
-    /* Compute distance from sample to each database entry */
-    match_decomposed(db, matches, N, sample);
+    for (int i = 0; i < N; i++) {
+        int distance = 0;
+        for (int j = 0; j < K; j++) {
+            int diff = db[i * K + j] - sample[j];
+            distance += diff * diff;
+        }
 
-    /* Find and return the minimum distance and index */
-    return min_with_index(matches, N);
+        if (i == 0 || distance < min_sum) {
+            min_sum = distance;
+            min_index = i;
+        }
+    }
+
+    result[0] = min_sum;
+    result[1] = min_index;
+    return result[0];
 }
